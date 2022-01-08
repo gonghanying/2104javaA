e4c5a9e625ec12f81b274b4c8cc7bcca65e94e20
hyw <3148570872@qq.com>
2022-01-08 11:20:24 +0800

    --[M]这是张三的测试修改

--- src/main/java/com/lening/EurekaApplication.java
+++ src/main/java/com/lening/EurekaApplication.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
-
+    /**
+     *这是测试者李四
+     */
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
